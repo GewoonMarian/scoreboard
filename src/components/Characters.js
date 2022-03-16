f542c3c71f0e76f0cbe6bbdd39f4400ddcd3a892
nextcode1 <stargateblk@gmail.com>
2022-03-16 19:19:39 +0100

    all features are done

---
+++ src/components/Characters.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+export default function Characters() {
+  const [characters, setCharacters] = useState();
+
+  //Step 1. Import Axios
+  //Step 2. Write an async function to fetch the data
+  //Step 3. Make a request with axios
+  //Step 4. Import useEffect and call the function
+  //Step 5. Console.log the response
+  //Step 6. Declare a variable with local state to hold the data you fetched, ex: const [ characters, setCharacters ] = useState()
+  //Step 7. Put the data in local state using the setter, ex:  setCharacters(response.data.rows)
+  //STep 8. Map over the data
+
+  const getData = async () => {
+    const response = await axios.get(
+      "https://hp-assessment-api.herokuapp.com/got/characters"
+    );
+    console.log("response", response);
+    setCharacters(response.data.rows);
+  };
+
+  // getData() //this ends in an infinite loop
+  console.log(characters);
+
+  useEffect(() => {
+    //use Effect only runs once, when the page renderes
+    getData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Characters list</h1>
+      {!characters
+        ? //conditional rendering, in case something unexpected happens, like: slow internet, API breaks
+          "Loading"
+        : characters.map((char) => {
+            return (
+              <div>
+                <h3>{char.name}</h3>
+                <img style={{ width: 200 }} alt="Imag" src={char.imageUrl} />
+              </div>
+            );
+          })}
+    </div>
+  );
+}
---
+++ src/components/Scoreboard.js
@@ -0,0 +1,119 @@
+import { useState } from "react";
+import Player from "./Player";
+import AddPlayerForm from "./AddPlayerForm";
+
+const compareScore = (playerA, playerB) => {
+  return playerB.score - playerA.score;
+};
+
+const compareName = (playerA, playerB) => {
+  return playerA.name.localeCompare(playerB.name);
+};
+
+// TO DO
+//1. Create a card component for player
+//2. Make it possible to sort players by score and name
+
+export default function Scoreboard() {
+  const [players, setPlayers] = useState([
+    { id: 1, name: "Sam", score: 30 },
+    { id: 2, name: "Frodo", score: 10 },
+    { id: 3, name: "Bilbo", score: 40 },
+    { id: 4, name: "Merry & Pippin", score: 20 },
+  ]);
+
+  //Steps for sorting
+  // Step 1. Create a selector with the <select> tag
+  // Step 2. Add <option> tags and give each option a value (this way we can track the selected one)
+  // Step 3. Write a function to console.log the selected value
+  // Step 4. Pass the function to an onChange in the select tag (test it!, see if the console.log is right)
+  // Step 5. Create a local state to hold the selected value
+  // Step 6. Inside the function, use the setter to put the selected value in local state
+  // Step 7. Write a const that decides the sorting method
+  // Step 8. Map over that new const instead of [players]
+
+  const [sortBy, setSortBy] = useState("name");
+
+  const sortedPlayers =
+    sortBy === "name"
+      ? [...players].sort(compareName)
+      : [...players].sort(compareScore);
+
+  const select = (event) => {
+    // console.log(event.target.value)
+    setSortBy(event.target.value);
+  };
+
+  //Steps for Incremeting Score => Write a map that checks if that's the id we need, if so, we update, else not
+
+  const incrementScore = (id) => {
+    console.log("id", id);
+    const newPlayersArray = players.map((player) => {
+      if (player.id === id) {
+        return {
+          ...player,
+          score: player.score + 1,
+        };
+      } else {
+        return player;
+      }
+    });
+    console.log("new array", newPlayersArray);
+    setPlayers(newPlayersArray);
+  };
+
+  //Write a function to add a player to the array
+  const addPlayer = (name) => {
+    console.log("name", name);
+    //1. Define what is a new player
+    const newPlayer = { id: players.length + 1, name: name, score: 0 };
+    //2. Put the new player in the array
+    const newPlayersArray = [...players, newPlayer];
+    //3. Put new player in local state
+    setPlayers(newPlayersArray);
+  };
+
+  const resetScores = () => {
+    const new_players_array = players.map((player) => ({
+      ...player,
+      score: 0,
+    }));
+    setPlayers(new_players_array);
+  };
+
+  const randomize = () => {
+    const refresh = players.map((player) => ({
+      ...player,
+      score: Math.floor(Math.random() * 50),
+    }));
+
+    setPlayers(refresh);
+  };
+
+  return (
+    <div>
+      <h1>Scoreboard</h1>
+      Sort By:
+      <select onChange={select}>
+        <option value="name">name</option>
+        <option value="score">score</option>
+      </select>
+      <button onClick={resetScores}>Reset</button>
+      <button onClick={randomize}>Randomize</button>
+      {!players
+        ? "Loading"
+        : sortedPlayers.map((player) => {
+            return (
+              <Player
+                key={player.id}
+                name={player.name}
+                score={player.score}
+                incrementScore={incrementScore}
+                id={player.id}
+              />
+            );
+          })}
+      <AddPlayerForm addPlayer={addPlayer} />
+    </div>
+  );
+}
